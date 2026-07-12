2e1f9821c535bde94ae48e1e28014f128c05693c
Karthikjv27 <karthikeyan50506012@gmail.com>
2026-07-12 14:46:03 +0530

    changed

--- odoo/src/main/java/com/EcoSphere/odoo/dashboard/controller/DashboardController.java
+++ odoo/src/main/java/com/EcoSphere/odoo/dashboard/controller/DashboardController.java
@@ -2,6 +2,9 @@ package com.EcoSphere.odoo.dashboard.controller;
 
 import com.EcoSphere.odoo.department.service.DepartmentService;
 import com.EcoSphere.odoo.environmental.carbon.service.CarbonTransactionService;
+import com.EcoSphere.odoo.environmental.goal.service.EnvironmentalGoalService;
+import com.EcoSphere.odoo.governance.policy.service.PolicyService;
+import com.EcoSphere.odoo.social.csr.service.CSRActivityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,17 @@ public class DashboardController {
     private final ESGScoreService esgScoreService;
     private final DepartmentService departmentService;
     private final CarbonTransactionService carbonTransactionService;
-
+    private final CSRActivityService csrService;
+    private final PolicyService policyService;
+    private final EnvironmentalGoalService goalService;
     public DashboardController(ESGScoreService esgScoreService, DepartmentService departmentService,
-                               CarbonTransactionService carbonTransactionService) {
+                               CarbonTransactionService carbonTransactionService, CSRActivityService csrService, PolicyService policyService, EnvironmentalGoalService goalService) {
         this.esgScoreService = esgScoreService;
         this.departmentService = departmentService;
         this.carbonTransactionService = carbonTransactionService;
+        this.csrService = csrService;
+        this.policyService = policyService;
+        this.goalService = goalService;
     }
 
     @GetMapping("/")
@@ -32,6 +40,13 @@ public class DashboardController {
                 carbonTransactionService.getAll());
 
         model.addAttribute("score", esgScoreService.calculateScore());
+
+        model.addAttribute("csrCount", csrService.getAll().size());
+
+        model.addAttribute("policyCount", policyService.getAll().size());
+
+        model.addAttribute("goalCount", goalService.getAll().size());
+
         return "dashboard";
     }
 }
